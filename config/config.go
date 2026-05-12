569b6916b1f69c005cb4f875049484aff02812da
Satyam Singh <62979978+Satyam085@users.noreply.github.com>
2026-05-12 12:07:22 +0530

    Update config.go

--- config/config.go
+++ config/config.go
@@ -40,7 +40,7 @@ var DurationRules = []models.DurationRule{
 	{Label: "≤ 15 min", MaxHours: 0.25, ReasonID: 21},      // Jumper Touching (0-15 minutes)
 	{Label: "15 min–1 hr", MaxHours: 1, ReasonID: 20},      // Jumper Burnt (15 min - 1 hour)
 	{Label: "1–3 hours", MaxHours: 3, ReasonID: 31},        // Tree / Tree Branch Falling (1-3 hours)
-	{Label: "3–8 hours", MaxHours: 8, ReasonID: 9},         // Conductor Snapped HT Line (3-8 hours)
-	{Label: "~15.73 hours", MaxHours: 15.73, ReasonID: 25}, // No Cause found (exactly 15.73 hours)
+	{Label: "3–8 hours", MaxHours: 8, ReasonID: 74},         // Conductor Snapped HT Line (3-8 hours)
+	{Label: "~15.73 hours", MaxHours: 15.73, ReasonID: 28}, // No Cause found (exactly 15.73 hours)
 	// Any other duration will be skipped
 }
